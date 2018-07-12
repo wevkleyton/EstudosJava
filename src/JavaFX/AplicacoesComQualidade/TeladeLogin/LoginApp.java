dd686de188328645dd4965ac5d82b02eb36f9cdf
wev <kleyton.cpd@gmail.com>
2018-07-12 18:46:09 -0300

    Inclusão de ccs

--- src/JavaFX/AplicacoesComQualidade/TeladeLogin/LoginApp.java
+++ src/JavaFX/AplicacoesComQualidade/TeladeLogin/LoginApp.java
@@ -1,6 +1,5 @@
 package JavaFX.AplicacoesComQualidade.TeladeLogin;
 
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,32 +9,36 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import sun.awt.X11.XTrayIconPeer;
 
-public class LoginApp extends Application{
+public class LoginApp extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		AnchorPane anchorPane = new AnchorPane();
-		anchorPane.setPrefSize(400, 300);
+		AnchorPane pane = new AnchorPane();
+		pane.setPrefSize(400, 300);
 		TextField txLogin = new TextField();
 		txLogin.setPromptText("Digite seu Login");
-		txLogin.setLayoutX((anchorPane.getWidth() - txLogin.getWidth() ) / 2);
-		txLogin.setLayoutY(50);
 		PasswordField txPassword = new PasswordField();
 		txPassword.setPromptText("Digite aqui sua Senha");
-		txPassword.setLayoutX((anchorPane.getWidth() - txPassword.getWidth())/2);
-		txPassword.setLayoutY(100);
 		Button btEntrar = new Button("Entrar");
-		btEntrar.setLayoutX((anchorPane.getWidth() - btEntrar.getWidth())/2);
-		btEntrar.setLayoutY(150);
 		Button btSair = new Button("Sair");
-		btSair.setLayoutX((anchorPane.getWidth() - btSair.getWidth())/2);
-		btSair.setLayoutY(200);
-		anchorPane.getChildren().addAll(txLogin, txPassword, btEntrar, btSair);
-		Scene scene = new Scene(anchorPane);
+		pane.getChildren().addAll(txLogin, txPassword, btEntrar, btSair);
+		Scene scene = new Scene(pane);
 		primaryStage.setTitle("Tela de login");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+
+		txLogin.setLayoutX((pane.getWidth() - txLogin.getWidth()) / 2);
+		txLogin.setLayoutY(50);
+		txPassword.setLayoutX((pane.getWidth() - txPassword.getWidth()) / 2);
+		txPassword.setLayoutY(100);
+		btEntrar.setLayoutX((pane.getWidth() - btEntrar.getWidth()) / 2);
+		btEntrar.setLayoutY(150);
+		btSair.setLayoutX((pane.getWidth() - btSair.getWidth()) / 2);
+		btSair.setLayoutY(200);
+
+		pane.setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 100%, blue 0%, silver 100%);");
 	}
 
 	public static void main(String[] args) {
